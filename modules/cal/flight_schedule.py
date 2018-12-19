4a095a1848cac1106778dd5ab9a0bc9eb24bf53d
Randy Ram <rram@Randys-MacBook-Pro.local>
2018-12-19 17:37:52 -0400

    Added Ana flight schedule slash command handling

---
+++ modules/cal/flight_schedule.py
@@ -0,0 +1,76 @@
+import requests
+import datetime
+from pprint import pprint
+from modules.config import flight_schedule_endpoint
+from datetime import datetime
+
+"""
+[[{'arrival_city': 'YYZ',
+   'arrival_datetime': '171218T0640',
+   'departure_city': 'POS',
+   'departure_datetime': '171218T0135',
+   'flight_number': '602'}],
+ [{'arrival_city': 'YYZ',
+   'arrival_datetime': '171218T1815',
+   'departure_city': 'POS',
+   'departure_datetime': '171218T1310',
+   'flight_number': '600'}]]
+"""
+
+
+def make_flight_schedule_req(dept_date, dept_city, arrv_city, access_code):
+    data = {
+      "dept_date": dept_date,  # ddmmyy
+      "dept_city": dept_city,
+      "arrv_city": arrv_city,
+      "access_code": access_code
+    }
+    pprint(data)
+    resp = requests.post(flight_schedule_endpoint, json=data, verify=False)
+    pprint(resp.json())
+    return resp.json()
+
+
+def format_datetime(datetime_str):
+    return datetime.strptime(datetime_str, "%d%m%yT%H%M").strftime("%a %d %b, %Y %I:%M%p")
+
+
+def get_flight_schedule(date, dept_city, arrv_city, max_connections):
+    msg_to_send = ''
+    access_code = datetime.now().strftime("%A").lower()
+    date = datetime.strptime(date, "%Y%m%d").strftime("%d%m%y")
+    schedule = make_flight_schedule_req(date, dept_city, arrv_city, access_code)
+    if 'status' in schedule and schedule['status'] is True:
+        for flights in schedule['flights']:
+            if len(flights) > int(max_connections) + 1:
+                continue
+            for index, each_flight in enumerate(flights):
+                if len(flights) == 1:  # Direct flight
+                    msg_to_send += f"*{each_flight['departure_city']} -> {each_flight['arrival_city']}*\n" \
+                                   f"*BW{each_flight['flight_number']}*\n" \
+                                   f"Dept Time: {format_datetime(each_flight['departure_datetime'])}\n" \
+                                   f"Arrv Time: {format_datetime(each_flight['arrival_datetime'])}\n{'*' * 40}\n\n"
+                else:
+                    flight_string = ''
+                    local_msg = ''
+                    for index2, each_leg in enumerate(flights):
+                        # print(flights)
+                        if index2 == 0:
+                            flight_string += f"*{each_leg['departure_city']}->"
+                        elif index2 == len(flights) - 1 and len(flights) == 2:
+                            flight_string += f"{each_leg['departure_city']}->{each_leg['arrival_city']}*\n"
+                        elif index2 == len(flights) - 1:
+                            flight_string += f"->{each_leg['arrival_city']}*\n"
+                        else:
+                            flight_string += f"{each_leg['departure_city']}->{each_leg['arrival_city']}"
+                        local_msg += f"*{each_leg['departure_city']} -> {each_leg['arrival_city']}*\n" \
+                                     f"*BW{each_leg['flight_number']}*\n" \
+                                     f"Dept Time: {format_datetime(each_leg['departure_datetime'])}\n" \
+                                     f"Arrv Time: {format_datetime(each_leg['arrival_datetime'])}\n\n"
+                    msg_to_send += f"{flight_string}\n{local_msg}\n{'*' * 40}\n\n"
+        return msg_to_send
+
+
+if __name__ == "__main__":
+    get_flight_schedule("20190112", "POS", "JFK")
+
--- modules/slack/slack_dispatcher.py
+++ modules/slack/slack_dispatcher.py
@@ -5,6 +5,7 @@ Dispatcher for slack slash commands
 from modules.slack.slack_core import send_response_to_slash_command
 from modules.cal.flight_staff_listing import get_flight_staff_listing
 from modules.cal.flight_loads import get_flight_loads_command
+from modules.cal.flight_schedule import get_flight_schedule
 from pprint import pprint
 
 """
@@ -44,6 +45,17 @@ def handle_flight_load(response_url, text):
                                                      "*/fload 20181228 525*")
 
 
+def handle_flight_schedule(response_url, text):
+    try:
+        date, dept_city, arrv_city, connections = text.strip().split()
+        resp = get_flight_schedule(date, dept_city, arrv_city, connections)
+        pprint(resp)
+        send_response_to_slash_command(response_url, resp)
+    except ValueError:
+        send_response_to_slash_command(response_url, "Improper command usage. Please ensure you use the command like:"
+                                                     "*/fsched 20181228 POS JFK 0*")
+
+
 def dispatch_slack_action(slack_request):
     command = slack_request['command']
     response_url = slack_request['response_url']
@@ -52,3 +64,5 @@ def dispatch_slack_action(slack_request):
         handle_staff_flight_list(response_url, text)
     elif command == "/fload":
         handle_flight_load(response_url, text)
+    elif command == "/fsched":
+        handle_flight_schedule(response_url, text)
